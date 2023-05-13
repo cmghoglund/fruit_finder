ba847bd218b58b9d563be26add0d6a41952d5554
cmghoglund <cmghoglund@gmail.com>
2023-05-13 18:45:15 +0900

    Cleaned up welcome message and instructions. Added first variables for game logic.

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ print("\nWelcome to the Banana Finder!")
 print("\nWith this exciting game, you can now waste away hours of your life... Have fun!!")
 
 player_name = input("\nFirst things first... Please tell me your name: ")
-print("\nI have good news for you, " + player_name + ".")
-print("You've just been transformed into an ape, and you now face a very important task:")
-print("\nFIND A BANANA!")
+print("\nI have both good and bad news for you, " + player_name + ".")
+print("The bad news is, you've just been transformed into an ape. (Yeah, I told you it was bad.)")
+print("The good news is, you now face a very important task:")
+print("\n\tFIND A BANANA!")
 print("\nAs you'll soon find out, this is not as simple as it sounds.")
 print("You see, " + player_name + ", a delicious banana has been cleverly hidden behing one of the following five doors.")
-print("Your task is to guess behind which door.\n")
+print("Your task is to guess behind which one.\n")
+print("Got it? Okay, here we go...\n")
 
 print("|X| |X| |X| |X| |X|")
-print()
+
+banana_location = 1
+
+guessed_location = input("\nSo... Which door is it?? Choose 1-5: ")
