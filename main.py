c67ce4f00b3d4fab62f0b448e914412f51c7dcaa
cmghoglund <cmghoglund@gmail.com>
2023-05-13 18:18:56 +0900

    First version of the game with only the welcome message and instructions.

---
+++ main.py
@@ -0,0 +1,13 @@
+print("\nWelcome to the Banana Finder!")
+print("\nWith this exciting game, you can now waste away hours of your life... Have fun!!")
+
+player_name = input("\nFirst things first... Please tell me your name: ")
+print("\nI have good news for you, " + player_name + ".")
+print("You've just been transformed into an ape, and you now face a very important task:")
+print("\nFIND A BANANA!")
+print("\nAs you'll soon find out, this is not as simple as it sounds.")
+print("You see, " + player_name + ", a delicious banana has been cleverly hidden behing one of the following five doors.")
+print("Your task is to guess behind which door.\n")
+
+print("|X| |X| |X| |X| |X|")
+print()
