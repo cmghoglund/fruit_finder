b1c610c4cb98c4167bec2413b44908c5001f2a54
cmghoglund <cmghoglund@gmail.com>
2023-05-13 19:39:54 +0900

    Reformatted welcome message so it stands out more. Changed string formatting to use f-strings where applicable. Fixed spelling error in instructions.

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import random
 
 number_of_doors = 5
 
-print("\nWelcome to the Banana Finder!")
+print()
+game_title = "■ Welcome to the Banana Finder! ■"
+print("■" * len(game_title))
+print(game_title)
+print("■" * len(game_title))
+
 print("\nWith this exciting game, you can now waste away hours of your life... Have fun!!")
 
 player_name = input("\nFirst things first... Please tell me your name: ")
-print("\nI have both good and bad news for you, " + player_name + ".")
+print(f"\nI have both good and bad news for you, {player_name}.")
 print("The bad news is, you've just been transformed into an ape. (Yeah, I told you it was bad.)")
 print("The good news is, you now face a very important task:")
 print("\n\tFIND A BANANA!")
 print("\nAs you'll soon find out, this is not as simple as it sounds.")
-print("You see, " + player_name + ", a delicious banana has been cleverly hidden behing one of the following five doors.")
+print(f"You see, {player_name} , a delicious banana has been cleverly hidden behind one of the following five doors.")
 print("Your task is to guess behind which one.\n")
 print("Got it? Okay, here we go...\n")
 
@@ -31,5 +36,5 @@ while keep_guessing:
     else:
         print("\nOops, no banana. But hunger is a great motivator!")
 
-print("\nMany thanks for playing! It was lots of fun, wasn't it?!")
+print(f"\nMany thanks for playing, {player_name}! It was lots of fun, wasn't it?!")
 print("Just don't tell anyone how many hours you wasted trying to find the banana...\n")
